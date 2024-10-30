304f7d9dbb8a434b482772d9f6f822eb6fa73da9
Shaurya Vardhan Singh <gsshaurya@gmail.com>
2024-10-30 15:44:12 +0530

    feat: add usdc token address for chains

---
+++ src/constants/usdcAddresses.ts
@@ -0,0 +1,31 @@
+export const UsdcAddresses: {
+  name: string
+  chainId: 42161 | 43114 | 8453 | 1 | 137
+  tokenAddress: `0x${string}`
+}[] = [
+  {
+    name: "arbitrum",
+    chainId: 42161,
+    tokenAddress: "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+  },
+  {
+    name: "avalanche",
+    chainId: 43114,
+    tokenAddress: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+  },
+  {
+    name: "base",
+    chainId: 8453,
+    tokenAddress: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  },
+  {
+    name: "mainnet",
+    chainId: 1,
+    tokenAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  },
+  {
+    name: "polygon",
+    chainId: 137,
+    tokenAddress: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+  },
+]
